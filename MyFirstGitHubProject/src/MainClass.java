873db95bea879d8285b3c77e3b31d7e4518642d3
sanjanachandel92 <sanjanachandel92@gmail.com>
2023-03-22 22:29:39 -0400

    Added my first java project in github

---
+++ MyFirstGitHubProject/src/MainClass.java
@@ -0,0 +1,10 @@
+public class MainClass {
+    public static void main(String args[]) {
+        printHello();
+    }
+
+    public static void printHello() {
+
+        System.out.println("Hello World");
+    }
+}
