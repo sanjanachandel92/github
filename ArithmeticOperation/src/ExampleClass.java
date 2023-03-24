9af2faf53266fcdbe5df85b0cbf94c44167ab646
sanjanachandel92 <sanjanachandel92@gmail.com>
2023-03-23 21:25:08 -0400

    Arithmetic operation proj added

---
+++ ArithmeticOperation/src/ExampleClass.java
@@ -0,0 +1,17 @@
+import java.net.SocketOption;
+
+public class ExampleClass {
+    public static void main(String[] args){
+
+        printHelloMyName ("Sanjana");
+    }
+    public static void printHelloWorld() {
+
+        System.out.println("Hello World");
+    }
+
+    public static void printHelloMyName(String str) {
+
+        System.out.println("Hello " +str);
+    }
+}
---
+++ ArithmeticOperation/src/MainClass.java
@@ -0,0 +1,39 @@
+public class MainClass {
+
+    public static void main(String args[])
+    {
+
+        System.out.println(sumOfTwoNumbers(2,3));
+        System.out.println(diffOfTwoNumbers(2,3));
+        System.out.println(prodOfTwoNumbers(2,3));
+        System.out.println(quoOfTwoNumbers(8,2));
+        System.out.println(remainderOfTwoNumbers(8,2));
+
+}
+
+    public static int sumOfTwoNumbers(int a, int b)
+    {
+
+        return (a + b);
+    }
+    public static int diffOfTwoNumbers(int a, int b)
+    {
+
+        return (a - b);
+    }
+
+    public static int prodOfTwoNumbers(int a, int b)
+    {
+
+        return (a * b);
+    }
+    public static int quoOfTwoNumbers(int a, int b)
+    {
+
+        return (a / b);
+    }
+
+    public static int remainderOfTwoNumbers(int a, int b)
+    {
+        return (a % b);
+    }
